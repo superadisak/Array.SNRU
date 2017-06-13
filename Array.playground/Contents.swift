c0796cd96185f4976c1f40f01499f70e1fec84b6
Student18 <axiom2517@gmail.com>
2017-06-13 13:41:25 +0700

    print

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// การใช้ตัวแปร Array
+// การประกาศตัวแปร Array  และกำหนดค่า
+let intArrayNumber1 = [11,22,33,44]
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [55,66,77,88]
+
+//String Type
+var strArrayName1 = ["Doremon","Nobita", "Master Ung",]
+
+var strArrayName2 = [String]()
+strArrayName2 = ["AAA","BBB","CCC"]
+
+//Double
+var douArrayNum3 = [1.2, 1.3,1.4,]
+var douArrayNub4 = [Double]()
+douArrayNub4 = [2.1,2.2,2.3]
+
+// การเพ่ิมสมาชิกให้ Array
+print("intArrayNumber2 ==> \(intArrayNumber2)")
+intArrayNumber2.append(111)
+
+strArrayName2.append("Doremon")
+
+
+
+
+
+
+
+
